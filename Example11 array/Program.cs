fa36c07cf17833e0d9bc2954b77807dd271fb732
Aleksandr <aleksandr-s86>
2023-02-21 22:30:20 +0300

    Методы

---
+++ Example11 array/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillAArray(int[]collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while(index<lenght)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Indexof(int[] collection,int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index<count)
+    {
+        if(collection[index]==find)
+        {
+            position=index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int [] array=new int[10];
+
+FillAArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof(array, 44);
+Console.WriteLine(pos);
